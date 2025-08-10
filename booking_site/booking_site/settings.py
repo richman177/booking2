62281a6c5d9cb628e88e85949aa7b33cd42e07bb
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-08-10 17:52:28 +0600

    Update settings.py
    Df

--- booking_site/booking_site/settings.py
+++ booking_site/booking_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/ 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
